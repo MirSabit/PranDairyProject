59923df8c030146d4c09b7b8f4edcde8cc5fdebc
MirSabit <124068787+MirSabit@users.noreply.github.com>
2026-07-13 22:13:48 +0600

    Add files via upload

---
+++ PranDairyProject/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.prandairyproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.prandairyproject to javafx.fxml;
+    exports com.example.prandairyproject;
+}
