47169cee414226916afd4b13512d6049332d0c51
Tasmima Sultana Mim <2310424@iub.edu.bd>
2026-07-30 00:53:15 +0600

    Edited

--- PranDairyProject/src/main/java/QC/QCDashboardController.java
+++ PranDairyProject/src/main/java/QC/QCDashboardController.java
@@ -4,7 +4,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-
 public class QCDashboardController {
 
     @FXML private Label lblWelcome;
@@ -20,42 +19,42 @@ public class QCDashboardController {
 
     @FXML
     private void initialize() {
+
     }
 
     @FXML
     private void handleLogout() {
-        // TODO: implement btnLogout action
     }
     @FXML
     private void handleGoal1() {
-        // TODO: implement btnGoal1 action
+
     }
     @FXML
     private void handleGoal2() {
-        // TODO: implement btnGoal2 action
+
     }
     @FXML
     private void handleGoal3() {
-        // TODO: implement btnGoal3 action
+
     }
     @FXML
     private void handleGoal4() {
-        // TODO: implement btnGoal4 action
+
     }
     @FXML
     private void handleGoal5() {
-        // TODO: implement btnGoal5 action
+
     }
     @FXML
     private void handleGoal6() {
-        // TODO: implement btnGoal6 action
+
     }
     @FXML
     private void handleGoal7() {
-        // TODO: implement btnGoal7 action
+
     }
     @FXML
     private void handleGoal8() {
-        // TODO: implement btnGoal8 action
+
     }
 }
